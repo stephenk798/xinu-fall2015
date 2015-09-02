c654c8067ab991b12c7b0b776763938287ae9285
Stephen W. Kramer <kramer40@xinu19.cs.purdue.edu>
2015-09-02 14:02:19 -0400

    added xinu files

---
+++ system/main.c
@@ -0,0 +1,26 @@
+/*  main.c  - main */
+
+#include <xinu.h>
+#include <stdio.h>
+
+process	main(void)
+{
+	/*Moved hello world message to intialize.c on 9/1/15*/
+	/*added create lab0app1.c and lab0app2.c*/
+	resume(create((void *)lab0app1, 65536, 20, "lab0 app1", 0, NULL));
+	
+	/*-------*/
+	kprintf("\n...creating a shell\n");
+	recvclr();
+	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
+
+	/* Wait for shell to exit and recreate it */
+
+	while (TRUE) {
+		receive();
+		sleepms(200);
+		kprintf("\n\nMain process recreating shell\n\n");
+		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
+	}
+	return OK;
+}
