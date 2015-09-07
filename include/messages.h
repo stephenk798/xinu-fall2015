e46a1489930cdbc4d94c67a78ae085b5a8dddaa9
Stephen Kramer <stephenk798@gmail.com>
2015-09-07 16:48:20 -0400

    maybe fix printing

--- include/messages.h
+++ include/messages.h
@@ -1,11 +1,11 @@
 /*created 9/7/15 for random string constants I may need*/
 /* WElcome banner (assumes VT100) */
 
-#define	WELCOME_BAN0	"\033[1;34m"
+#define	WELCOME_BAN0	  "\033[1;34m"
 #define WELCOME_BAN1      "------------------------------------"
 #define WELCOME_BAN2      "   _________________  __ _______  __"
 #define WELCOME_BAN3      "  / __/_  __/ __/ _ \\/ // / __/ |/ /"
 #define WELCOME_BAN4      " _\\ \\  / / / _// ___/ _  / _//    / "
 #define WELCOME_BAN5      "/___/ /_/ /___/_/  /_//_/___/_/|_/  "
 #define WELCOME_BAN6      "------------------------------------"
-#define	WELCOME_BAN7	  "\033[0;39m\n"
+#define	WELCOME_BAN7	  "\033[0;97m\n"
