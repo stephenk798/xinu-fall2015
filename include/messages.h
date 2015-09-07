16eeab5f90cc8455139e93ebc0889de82e95c327
Stephen Kramer <stephenk798@gmail.com>
2015-09-07 15:45:11 -0400

    added new welcome message

---
+++ include/messages.h
@@ -0,0 +1,10 @@
+/* Shell banner (assumes VT100) */
+
+#define	WELCOME_BAN0	"\033[1;34m"
+#define WELCOME_BAN1      "------------------------------------"
+#define WELCOME_BAN2      "   _________________  __ _______  __"
+#define WELCOME_BAN3      "  / __/_  __/ __/ _ \\/ // / __/ |/ /"
+#define WELCOME_BAN4      " _\\ \\  / / / _// ___/ _  / _//    / "
+#define WELCOME_BAN5      "/___/ /_/ /___/_/  /_//_/___/_/|_/  "
+#define WELCOME_BAN6      "------------------------------------"
+#define	WELCOME_BAN7	  "\033[0;39m\n"
