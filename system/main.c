57f65b97284a549b1ca6c50f7ff7525f58cb420a
Stephen Kramer <stephenk798@gmail.com>
2015-09-07 16:35:13 -0400

    possibly fixed messed up printing

--- system/main.c
+++ system/main.c
@@ -8,14 +8,15 @@ process	main(void)
 	/*Moved hello world message to intialize.c on 9/1/15*/
 
 
+	/*added create lab0app1.c and lab0app2.c 9/7/15*/
+	resume(create(lab0app1, 65536, 20, "lab0 app1", 0, NULL));
+	resume(create(lab0app2, 65536, 20, "lab0 app2", 0, NULL));
+	recvclr();
 	/*-------*/
 	kprintf("\n...creating a shell\n");
 	recvclr();
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
 	
-	/*added create lab0app1.c and lab0app2.c 9/7/15*/
-	resume(create(lab0app1, 65536, 20, "lab0 app1", 0, NULL));
-	resume(create(lab0app2, 65536, 20, "lab0 app2", 0, NULL));
 
 	/* Wait for shell to exit and recreate it */
 
