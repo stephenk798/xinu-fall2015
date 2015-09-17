d99a90765d3fc28666a261d8e71ce768f33e4e24
Stephen Kramer <stephenk798@gmail.com>
2015-09-17 10:00:41 -0400

    semicolons are hard

--- system/printsegaddress.c
+++ system/printsegaddress.c
@@ -1,5 +1,5 @@
 #include <xinu.h>
 void printsegaddress(){
-	kprintf("Text\n Beginning: %8x\n End: %8x\n", &text, &etext - 1)
+	kprintf("Text\n Beginning: %8x\n End: %8x\n", &text, &etext - 1);
 	kprintf("Data: %8x\n Beginning: %8x\n End: %8x\n",&data, &etext, &edata - 1);
 }
