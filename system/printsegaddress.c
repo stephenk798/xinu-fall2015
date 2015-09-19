2b13df4f642805c86c02db87722bcd58e09c24cd
Stephen Kramer <stephenk798@gmail.com>
2015-09-19 15:06:34 -0400

    test code

--- system/printsegaddress.c
+++ system/printsegaddress.c
@@ -1,5 +1,5 @@
 #include <xinu.h>
 void printsegaddress(){
-	kprintf("Text\n Beginning: %8x\n End: %8x\n", &text, &etext - 1);
+	kprintf("Text\n Text: 0x%8x \n addTrest: %8x\n Beginning: %8x\n End: %8x\n", text, &text, etext, &etext - 1);
 	kprintf("Data: %8x\n Beginning: %8x\n End: %8x\n",&data, &etext, &edata - 1);
 }
