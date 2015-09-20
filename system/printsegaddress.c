6e4e71a0d6011c05f0dfa51c8f00d02424eb1788
Stephen Kramer <stephenk798@gmail.com>
2015-09-20 14:14:35 -0400

    added data

--- system/printsegaddress.c
+++ system/printsegaddress.c
@@ -1,5 +1,7 @@
 #include <xinu.h>
 void printsegaddress(){
 	kprintf("Text: 0x%08X\n Etext: 0x%08X\n &text: 0x%08X\n &etext: 0x%08X\n &etext-1: 0x%08X\n *(etext-1): 0x%08X\n *(&etext): 0x%08X\n", (uint32)text, (uint32)etext, (uint32)&text, (uint32)&etext, (uint32)(&etext-1),(uint32)*(&etext-1), (uint32)*(&etext));
+	kprintf("data: 0x%08X\n Edata: 0x%08X\n &data: 0x%08X\n &edata: 0x%08X\n &edata-1: 0x%08X\n *(edata-1): 0x%08X\n *(&edata): 0x%08X\n", (uint32)data, (uint32)edata, (uint32)&data, (uint32)&edata, (uint32)(&edata-1),(uint32)*(&edata-1), (uint32)*(&edata));
+
 	// kprintf("Text\n Value: 0x%08X \n End: 0x%08X\n Size: %10d\n", (uint32)*(&etext), (uint32)(&etext - 1), (uint32)(&etext - &text));
 }
