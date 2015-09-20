ec2a636dad6c61792fe0ad092b71cba25ce2b3d4
Stephen Kramer <stephenk798@gmail.com>
2015-09-20 14:02:53 -0400

    print stuff pls

--- system/printsegaddress.c
+++ system/printsegaddress.c
@@ -1,4 +1,5 @@
 #include <xinu.h>
 void printsegaddress(){
-	kprintf("Text\n Value: 0x%08X \n End: 0x%08X\n Size: %10d\n", (uint32)*(&etext), (uint32)(&etext - 1), (uint32)(&etext - &text));
+	kprintf("Text: 0x%08X\n Etext: 0x%08X\n &text: 0x%08X\n &etext: 0x%08X\n &etext-1: 0x%08X\n *(etext-1): 0x%08X\n *(&etext): 0x%08X\n", (uint32)text, (uint32)etext, (uint32)&text, (uint32)&etext, (uint32)(&etext-1),(uint32)*(&etext-1), (uint32)*(&etext));
+	// kprintf("Text\n Value: 0x%08X \n End: 0x%08X\n Size: %10d\n", (uint32)*(&etext), (uint32)(&etext - 1), (uint32)(&etext - &text));
 }
