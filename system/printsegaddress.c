504003b7df1b6b00a05c371e90ee8c41f1dd0b8d
Stephen Kramer <stephenk798@gmail.com>
2015-09-20 12:36:22 -0400

    maybe the right thing

--- system/printsegaddress.c
+++ system/printsegaddress.c
@@ -1,5 +1,5 @@
 #include <xinu.h>
 void printsegaddress(){
-	kprintf("Text\n Text: 0x%8x \n addTrest: %8x\n Beginning: %8x\n End: %8x\n", text, &text, etext, &etext - 1);
+	kprintf("Text\n Text: 0x%8x \n Beginning: 0x%8x\n End: 0x%8x\n Size: %d", text, &text, &etext - 1, &etext - &text);
 	kprintf("Data: %8x\n Beginning: %8x\n End: %8x\n",&data, &etext, &edata - 1);
 }
