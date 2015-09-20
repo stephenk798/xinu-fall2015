1a86bc7e23039015b1a107761298beff6e9eb575
Stephen Kramer <stephenk798@gmail.com>
2015-09-20 12:46:12 -0400

    part3.2 for lab 1

--- system/printsegaddress.c
+++ system/printsegaddress.c
@@ -1,5 +1,7 @@
 #include <xinu.h>
 void printsegaddress(){
-	kprintf("Text\n Text: 0x%8x \n Beginning: 0x%8x\n End: 0x%8x\n Size: %d", text, &text, &etext - 1, &etext - &text);
-	kprintf("Data: %8x\n Beginning: %8x\n End: %8x\n",&data, &etext, &edata - 1);
+	kprintf("Text\n Value: 0x%8x \n Beginning: 0x%8x\n End: 0x%8x\n Size: %d\n", (uint32)text, (uint32)&text, (uint32)(&etext - 1), (uint32)(&etext - &text);
+	kprintf("Data\n Value: 0x%8x \n Beginning: 0x%8x\n End: 0x%8x\n Size: %d\n", (uint32)data, (uint32)&data, (uint32)(&edata - 1), (uint32)(&edata - &data);
+	kprintf("Bss\n Value: 0x%8x \n Beginning: 0x%8x\n End: 0x%8x\n Size: %d\n", (uint32)bss, (uint32)&bss, (uint32)(&ebss - 1), (uint32)(&ebss - &bss);
+
 }
