7cc05fcf0c1731a0446eebad24abcacc0eb4faa7
Stephen Kramer <stephenk798@gmail.com>
2015-09-20 23:59:29 -0400

    test some arm stuff

--- system/stackdepth.c
+++ system/stackdepth.c
@@ -2,10 +2,18 @@
 #include<stdio.h>
 int stackdepth(){
 	unsigned long *top_esp, *top_ebp;
-	
+	unsigned long *cur_esp, *cur_ebp;
+
+	arm("mov [top_esp], esp"
+		"mov [top_ebp], ebp"
+		"mov[cur_esp], esp");
+	kprintf("cur_esp: 0x%08X\n", cur_esp);
 	int pid = getpid();
 	struct procent	*proc = &proctab[pid];
-	kprintf("prstkbase: 0x%08X\n", proc->prstkbase);
+	if(top_esp == proc->prstkbase){
+		kprintf("Stack Base: 0x%08X\n", proc->prstkbase);
+
+	}
 
 	return 0;
 }
