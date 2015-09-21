3c715e18c63e2ddaf90cf596964395c24f254ab6
Stephen Kramer <stephenk798@gmail.com>
2015-09-21 17:27:08 -0400

    super revamp pls

--- system/stackdepth.c
+++ system/stackdepth.c
@@ -5,19 +5,27 @@ static unsigned long	*ebp;
 
 int stackdepth(){
 	unsigned long *top_esp, *top_ebp;
-	top_esp = top_ebp-1;
-	
-	// asm("movl %esp, esp;");
-	// asm("movl %ebp, ebp;");
-	// kprintf("top_esp: 0x%08X\n", top_esp);
-	// kprintf("top_ebp: 0x%08X\n", top_ebp);
-	kprintf("------STACKTRACE------\n");
-	stacktrace(currpid);
+	int depth = 0;	
+	asm("movl %esp,esp;");
+	asm("movl %ebp,ebp;");
+
+	top_esp = esp;
+	top_ebp = ebp;
 	struct procent	*proc = &proctab[currpid];
-	if(top_esp == (unsigned long *)proc->prstkbase){
-		kprintf("Stack Base: 0x%08X\n", proc->prstkbase);
 
+	kprintf("------STACKTRACE------\n");
+	while(top_esp < (unsigned long *)proc->prstkbase){
+		kprintf("Stack Frame\n");
+		depth++;
+		kprintf("Count: %d\n", depth);
+		kprintf("ebp: 0x%08X\n", top_ebp);
+		kprintf("esp: 0x%08X\n", top_esp);
+		kprintf("size: %10d\n", top_ebp-top_esp);
+		top_esp = top_ebp+4;
+		top_ebp = (unsigned long *)(*top_ebp);
 	}
+	kprintf("Stack Base: 0x%08X\n", proc->prstkbase);
+	kprintf("Depth: %d\n", depth);
 
-	return 0;
+	return depth;
 }
