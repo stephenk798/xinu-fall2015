001536ef9763100dff8977d20c918810584811de
Stephen Kramer <stephenk798@gmail.com>
2015-09-21 15:14:15 -0400

    syntax fix?

--- system/stackdepth.c
+++ system/stackdepth.c
@@ -4,7 +4,7 @@ int stackdepth(){
 	unsigned long *top_esp, *top_ebp;
 	unsigned long *cur_esp, *cur_ebp;
 
-	asm("mov top_esp, esp");
+	asm("mov %esp, %top_esp");
 	// asm("mov top_ebp, ebp");
 	// asm("mov cur_esp, esp");
 	kprintf("cur_esp: 0x%08X\n", cur_esp);
