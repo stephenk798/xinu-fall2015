2af3cb83b0ba0fa8d1826f7b32ba74de834a0aca
Stephen Kramer <stephenk798@gmail.com>
2015-09-21 16:31:43 -0400

    maybe i belf

--- system/stackdepth.c
+++ system/stackdepth.c
@@ -1,15 +1,20 @@
-#include<xinu.h>
-#include<stdio.h>
+#include <xinu.h>
+#include <stdio.h>
+static unsigned long	*esp;
+static unsigned long	*ebp;
+
 int stackdepth(){
 	unsigned long *top_esp, *top_ebp;
-
-	asm("mov %%esp, %0;"
-		 "mov %%ebp, %1;"
-		:"=r"(top_esp), "=r" (top_ebp));
+	top_esp = top_ebp-1;
+	
+	asm("movl %esp, esp;");
+	asm("movl %ebp, ebp;");
 	kprintf("top_esp: 0x%08X\n", top_esp);
 	kprintf("top_ebp: 0x%08X\n", top_ebp);
+	kprintf("------STACKTRACE------\n");
+	stacktrace(currpid);
 	struct procent	*proc = &proctab[currpid];
-	if(top_esp == proc->prstkbase){
+	if(top_esp == (unsigned long *)proc->prstkbase){
 		kprintf("Stack Base: 0x%08X\n", proc->prstkbase);
 
 	}
