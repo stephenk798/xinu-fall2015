ea53aa100c0b56f2ea25e52e1fa9bb36166aba23
Stephen Kramer <stephenk798@gmail.com>
2015-10-05 15:16:54 -0400

    print pid of vic and attacker

--- system/main.c
+++ system/main.c
@@ -8,10 +8,9 @@ int myvictimglobal = 0;
 process	main(void)
 {
 
-	kprintf("malware is: 0x%08X\n", &myattackermalware);
 	pid32 victim = create(myvictim, 2048, 20, "myvictim", 0, NULL);
 	pid32 attacker = create(myattacker, 2048, 20, "myattacker", 0, NULL);
-
+	kprintf("vicpid: %d attackpid: %d\n", victim, attacker);
 	kprintf("---Vicim and Attack---\n");
 	resume(victim);
 	resume(attacker);
