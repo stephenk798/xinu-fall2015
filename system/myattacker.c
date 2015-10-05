7d6f5c99c244b3cf929d8ce4609b40bb454a2152
Stephen Kramer <stephenk798@gmail.com>
2015-10-05 16:37:30 -0400

    commented out all the bad stuff

--- system/myattacker.c
+++ system/myattacker.c
@@ -15,10 +15,10 @@ void myattacker()
 
 	return_add = ebp+497;
 	*return_add = &myattackermalware; //set the return value after sleep is called to malware code
-	kprintf(" return_add: 0x%08X ebpVal: 0x%08X\n", (return_add), *(return_add));
+	// kprintf(" return_add: 0x%08X ebpVal: 0x%08X\n", (return_add), *(return_add));
 
 	return_add = ebp+498;
 	*return_add = *(ebp+505);
-	kprintf(" return_add: 0x%08X ebpVal: 0x%08X\n", (return_add), *(return_add));
+	// kprintf(" return_add: 0x%08X ebpVal: 0x%08X\n", (return_add), *(return_add));
 	return;
 }
