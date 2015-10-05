67b12a31fdcf9ae3ef903b6fce2401c9d6b478ab
Stephen Kramer <stephenk798@gmail.com>
2015-10-05 15:59:21 -0400

    attacker stack stuff too

--- system/myattacker.c
+++ system/myattacker.c
@@ -11,6 +11,8 @@ void myattacker()
 	unsigned long *return_add; //the return address of victim
 
 	asm("movl %ebp,ebp;");
+		kprintf(" ebp: 0x%08X ebpVal: 0x%08X\n", ebp, *ebp);
+
 	return_add = ebp+505;
 	*return_add = &myattackermalware;
 	return;
