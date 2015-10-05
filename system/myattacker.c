5e565334a7d3553b7f9b59aa7beb842c34e78872
Stephen Kramer <stephenk798@gmail.com>
2015-10-05 10:47:46 -0400

    printing just ebp and esp for vic and attack

--- system/myattacker.c
+++ system/myattacker.c
@@ -9,6 +9,17 @@
 void myattacker()
 {
 	kprintf("----Attacker----\n");
-	stackdepth();
+	unsigned long *top_esp, *top_ebp;
+
+	int depth = 0;	
+
+	asm("movl %esp,top_esp;"); //get the esp and ebp into the global vars
+	asm("movl %ebp,top_ebp;");
+
+	// top_esp = esp; //and set local vars to the global ones
+	// top_ebp = ebp;
+	kprintf(" ebp: 0x%08X\n", top_ebp);
+	kprintf(" esp: 0x%08X\n", top_esp);
+	// stackdepth();
 	return;
 }
--- system/myvictim.c
+++ system/myvictim.c
@@ -22,14 +22,24 @@ int subsleep(){
 	int c;
 	c = a - b;
 	a = a - c;
-	kprintf("return address: 0x%08X\n", __builtin_return_address(0));
+	unsigned long *top_esp, *top_ebp;
+
+	int depth = 0;	
+
+	asm("movl %esp,top_esp;"); //get the esp and ebp into the global vars
+	asm("movl %ebp,top_ebp;");
+
+	// top_esp = esp; //and set local vars to the global ones
+	// top_ebp = ebp;
+	kprintf(" ebp: 0x%08X\n", top_ebp);
+	kprintf(" esp: 0x%08X\n", top_esp);
 	sleepms(2000);
 	return a;
 }
 void myvictim()
 {
 	kprintf("----Victim----\n");
-	stackdepth();
+	//stackdepth();
 	addnumbers();
 	subsleep();
 	kprintf("myvictimglobal: %d\n", myvictimglobal);  
