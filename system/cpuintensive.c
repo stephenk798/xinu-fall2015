482f4154fd9eebdc3a76efec7a0becdf664996ff
Stephen Kramer <stephenk798@gmail.com>
2015-10-06 22:48:20 -0400

    changed loop2 to smaller value because processes are interrupting mid print

--- system/cpuintensive.c
+++ system/cpuintensive.c
@@ -1,7 +1,7 @@
 #include <xinu.h>
 
 #define LOOP1 5
-#define LOOP2 234567 
+#define LOOP2 123456 
 
 void cpuintensive(){
 	int i,j;
@@ -12,9 +12,9 @@ void cpuintensive(){
 	    a *= 2;
 	    a--; 
 	  }
-	  kprintf("pid: %d, i: %d, priority:%d, preempt: %d\n", currpid, i, proctab[currpid].prprio, preempt);
+	  kprintf("\npid: %d, i: %d, priority:%d, preempt: %d\n", currpid, i, proctab[currpid].prprio, preempt);
 	}
-	kprintf("prcpuused: %d\n", proctab[currpid].prcpuused);
+	kprintf("\npid: %d prcpuused: %d\n", currpid, proctab[currpid].prcpuused);
 	// Print the CPU time consumed by the process that is recorded in the
 	// prcpuused field of the current process's process table entry.
 }
