2fa68730c629e922a811ee56ec19da67d3cf37a6
Stephen Kramer <stephenk798@gmail.com>
2015-10-22 09:29:37 -0400

    had to rename declarations

--- original/xinu-fall2015/include/multilevelfq.h
+++ original/xinu-fall2015/include/multilevelfq.h
@@ -2,6 +2,6 @@ struct mlfqueue { qid16	queues[DISPTBSIZE];	/* different level queues*/
 };
 
 extern struct mlfqueue mlfprocqueue;
-extern pid32 mlfqinsert(pid32, struct mlfqueue, int32);
-extern pid32 mlfqdequeue(struct mlfqueue);
-extern bool8 mlfqisempty(struct mlfqueue);
+extern pid32 mlfqinsert(pid32, struct mlfqueue*, int32);
+extern pid32 mlfqdequeue(struct mlfqueue*);
+extern bool8 mlfqisempty(struct mlfqueue*);
