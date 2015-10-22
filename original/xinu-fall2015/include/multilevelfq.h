90cb529f48d444dd6d77022e0edafddecba0ed98
Stephen Kramer <stephenk798@gmail.com>
2015-10-21 23:08:30 -0400

    forgot struct this entire time

--- original/xinu-fall2015/include/multilevelfq.h
+++ original/xinu-fall2015/include/multilevelfq.h
@@ -1,7 +1,7 @@
 struct mlfqueue { qid16	queues[DISPTBSIZE];	/* different level queues*/
 };
 
-extern mlfqueue mlfprocqueue;
+extern struct mlfqueue mlfprocqueue;
 extern pid32 mlfqinsert(mlfqueue, pid32, int32);
 extern pid32 mlfqdequeue(mlfqueue);
 extern bool mlfqisempty(mlfqueue);
