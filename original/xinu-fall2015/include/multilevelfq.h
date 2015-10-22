0127416f833778908f3a0fdfe5f9757cea919a81
Stephen Kramer <stephenk798@gmail.com>
2015-10-21 23:11:40 -0400

    doesnt even know what a bool is

--- original/xinu-fall2015/include/multilevelfq.h
+++ original/xinu-fall2015/include/multilevelfq.h
@@ -4,4 +4,4 @@ struct mlfqueue { qid16	queues[DISPTBSIZE];	/* different level queues*/
 extern struct mlfqueue mlfprocqueue;
 extern pid32 mlfqinsert(struct mlfqueue, pid32, int32);
 extern pid32 mlfqdequeue(struct mlfqueue);
-extern bool mlfqisempty(struct mlfqueue);
+extern bool8 mlfqisempty(struct mlfqueue);
