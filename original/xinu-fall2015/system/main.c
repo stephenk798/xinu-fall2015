56fc372e8c3c4da58c6aba3cdca91fdb20243743
Stephen Kramer <stephenk798@gmail.com>
2015-10-23 16:00:33 -0400

    reverse call order of cpu and io

--- original/xinu-fall2015/system/main.c
+++ original/xinu-fall2015/system/main.c
@@ -9,10 +9,10 @@ process	main(void)
 	// pid32 attacker = create(myattacker, 2048, 20, "myattacker", 0, NULL);
 	// resume(victim);
 	// resume(attacker);
-	pid32 procA = create(cpuintensive, 1024, 1, "procA", 0, NULL);
-	pid32 procB = create(cpuintensive, 1024, 1, "procB", 0, NULL);
-	pid32 procC = create(iointensive, 1024, 1, "procC", 0, NULL);
-	pid32 procD = create(iointensive, 1024, 1, "procD", 0, NULL);
+	pid32 procA = create(iointensive, 1024, 1, "procA", 0, NULL);
+	pid32 procB = create(iointensive, 1024, 1, "procB", 0, NULL);
+	pid32 procC = create(cpuintensive, 1024, 1, "procC", 0, NULL);
+	pid32 procD = create(cpuintensive, 1024, 1, "procD", 0, NULL);
 
 	
 	resume(procA);
