762cd67619ffaf35dce93f02e8b3200d666dd54a
Stephen Kramer <stephenk798@gmail.com>
2015-11-07 15:07:50 -0500

    Revert "added global time code"
    This reverts commit 017aed51e18077b782d152f8e3bf887bb762a7b2.

--- lab4/xinu-fall2015/system/clkhandler.c
+++ lab4/xinu-fall2015/system/clkhandler.c
@@ -11,8 +11,7 @@ void	clkhandler()
 	static	uint32	count1000 = 1000;	/* Count to 1000 ms	*/
 
 	/* Decrement the ms counter, and see if a second has passed */
-	clktimefine++;
-	
+
 	if((--count1000) <= 0) {
 
 		/* One second has passed, so increment seconds count */
--- lab4/xinu-fall2015/system/clkinit.c
+++ lab4/xinu-fall2015/system/clkinit.c
@@ -3,9 +3,7 @@
 #include <xinu.h>
 
 uint32	clktime;		/* Seconds since boot			*/
-uint32 clktimefine;		/* Milliseconds since boot		*/
 uint32	ctr1000 = 0;		/* Milliseconds since boot		*/
-uint32 clktimeswitch; 	/*time a process was switched in 	*/
 qid16	sleepq;			/* Queue of sleeping processes		*/
 uint32	preempt;		/* Preemption counter			*/
 
@@ -28,7 +26,6 @@ void	clkinit(void)
 	/* Initialize the time since boot to zero */
 
 	clktime = 0;
-	clktimefine = 0;
 
 	/* Set interrupt vector for the clock to invoke clkdisp */
 
