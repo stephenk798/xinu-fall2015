ee6f53912b3fa248c53ee36a7b2ad93beace2e26
Stephen Kramer <stephenk798@gmail.com>
2015-11-11 14:41:52 -0500

    added pdf to system

--- lab4/xinu-fall2015/system/resched.c
+++ lab4/xinu-fall2015/system/resched.c
@@ -49,7 +49,6 @@ void	resched(void)		/* Assumes interrupts are disabled	*/
 	ptcb = &proctab[currpid];
 	//Only call back if there is a message
 	if (ptcb->prhasmsg == TRUE && ptcb->cbfunc != NULL){
-		kprintf("calling cb func\n");
 		(*ptcb->cbfunc)();
 	}
 
