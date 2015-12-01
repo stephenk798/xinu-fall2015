8aae8a3826a9118daf00daa41997c7d82d57bf1d
Stephen Kramer <stephenk798@gmail.com>
2015-11-30 20:18:59 -0500

    prints were messed up

--- lab5/system/getmem.c
+++ lab5/system/getmem.c
@@ -33,7 +33,7 @@ char  	*getmem(
 			memlist.mlength -= nbytes;
 
 			gbgptr = &gbglist; //get the allocated list
-			kprintf("curr addy: 0x%08X, next addy: 0x%08X\n", curr, gbgptr, gbgptr->gbgnext);
+			kprintf("curr addy: 0x%08X, next addy: 0x%08X\n", curr, gbgptr->gbgnext);
 			gbglist.mlength += nbytes;
 			curr->mlength = nbytes;
 			curr->gbgpid = currpid; //Set the pid that owns the block
@@ -62,7 +62,7 @@ char  	*getmem(
 			memlist.mlength -= nbytes;
 			
 			gbgptr = &gbglist; //get the allocated list
-			kprintf("curr addy: 0x%08X, next addy: 0x%08X\n", curr, gbgptr, gbgptr->gbgnext);
+			kprintf("curr addy: 0x%08X, next addy: 0x%08X\n", curr, gbgptr->gbgnext);
 			gbglist.mlength += nbytes;
 			curr->mlength = nbytes; //set the length of the block to actual size
 			curr->gbgpid = currpid; //Set the pid that owns the block
