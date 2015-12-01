8c3170800c5ef012106f88fe99dba233c4ddb5dc
Stephen Kramer <stephenk798@gmail.com>
2015-11-30 19:55:35 -0500

    changed some more print of address

--- lab5/system/getmem.c
+++ lab5/system/getmem.c
@@ -32,7 +32,7 @@ char  	*getmem(
 			memlist.mlength -= nbytes;
 
 			gbgptr = &gbglist; //get the allocated list
-			kprintf("gbglistaddy: 0x%08X, next addy: 0x%08X\n", gbgptr, gbgptr->gbgnext);
+			kprintf("curr addy: 0x%08X, gbglistaddy: 0x%08X, next addy: 0x%08X\n", curr, gbgptr, gbgptr->gbgnext);
 			gbglist.mlength += nbytes;
 			curr->gbgpid = currpid; //Set the pid that owns the block
 			curr->gbgnext = gbgptr->gbgnext;//set the next gbg mem blk in list for current 
@@ -59,7 +59,7 @@ char  	*getmem(
 			memlist.mlength -= nbytes;
 			
 			gbgptr = &gbglist; //get the allocated list
-			kprintf("gbglistaddy: 0x%08X, next addy: 0x%08X\n", gbgptr, gbgptr->gbgnext);f
+			kprintf("curr addy: 0x%08X, gbglistaddy: 0x%08X, next addy: 0x%08X\n", curr, gbgptr, gbgptr->gbgnext);
 			gbglist.mlength += nbytes;
 			//curr->mlength = nbytes; //set the length of the block to actual size
 			curr->gbgpid = currpid; //Set the pid that owns the block
